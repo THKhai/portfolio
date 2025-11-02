a181336c1e8c1072956a55253e6c124f08eacbc1
KhaiTR <thkhai0912@gmail.com>
2025-11-02 17:13:52 +0700

    commit animation of the navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,7 +13,7 @@ import NavBar from "./NavBar.jsx";
 
 
 const Header = () => {
-    const [navOpen, setNavOpen] = useState(false);
+    const [navOpen, setNavOpen] = useState(true);
     return (
         <header className={"fixed top-0 left-0 w-full h-20 flex items-center z-40 bg-gradient-to-b from-zinc-900 to-zinc-900/0"}>
             <div className="max-w-screen-2xl w-full mx-auto px-4 flex justify-between items-center md:px-6 md:grid md:grid-cols-[1fr_3fr_1fr]">
@@ -29,12 +29,13 @@ const Header = () => {
                             />
                     </a>
                 </h1>
-                <div className="relative md:jusstify-self-center">
+                <div className="relative md:justify-self-center">
                     <button
-                        className="menu-btn md:hidden "
+                        className="menu-btn md:hidden"
                         onClick={() => setNavOpen((prev) => !prev)}
                     >
-                        <span className="material-symbols-rounded">menu
+                        <span className="material-symbols-rounded">
+                            {navOpen ? 'close' : 'menu'}
                         </span>
                     </button>
                     <NavBar navOpen={navOpen}/>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,12 +7,32 @@
  * Node modules
  * */
 import React from 'react';
-import { useRef } from 'react';
+import { useRef, useEffect } from 'react';
 import PropTypes  from 'prop-types';
 
 const NavBar = ({navOpen}) => {
     const lastActiveLink = useRef();
     const activeBox = useRef();
+
+    const initActiveBox = () => {
+        activeBox.current.style.top = lastActiveLink.current.offsetTop + 'px';
+        activeBox.current.style.left = lastActiveLink.current.offsetLeft + 'px';
+        activeBox.current.style.width = lastActiveLink.current.offsetWidth + 'px';
+        activeBox.current.style.height = lastActiveLink.current.offsetHeight + 'px';
+    }
+    useEffect(initActiveBox, [])
+    window.addEventListener('resize', initActiveBox);
+
+    const activeCurrentLink = (event) => {
+        lastActiveLink.current?.classList.remove('active');
+        event.target.classList.add('active');
+        lastActiveLink.current = event.target;
+
+        activeBox.current.style.top = event.target.offsetTop + 'px';
+        activeBox.current.style.left = event.target.offsetLeft + 'px';
+        activeBox.current.style.width = event.target.offsetWidth + 'px';
+        activeBox.current.style.height = event.target.offsetHeight + 'px';
+    }
     const navItems = [
         {
             label: 'Home',
@@ -42,7 +62,7 @@ const NavBar = ({navOpen}) => {
         }
     ];
     return (
-        <nav className={"navbar" + (navOpen ? '-active' : '')}>
+        <nav className={'navbar ' + (navOpen ? 'active' : '')}>
             {
                 navItems.map(({ label, link, className, ref},key) => (
                         <a
@@ -50,7 +70,7 @@ const NavBar = ({navOpen}) => {
                             href={link}
                             className={className}
                             ref={ref}
-                            onClick={null}
+                            onClick={activeCurrentLink}
                         >
                             {label}
                         </a>
